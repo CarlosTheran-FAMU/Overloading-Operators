35c5673964039ab505fb56978b89ba83236eaee1
carlos.theran@famu.edu <carlos.theran@famu.edu>
2026-04-08 11:28:53 -0700

    order fixed

--- overload.cpp
+++ overload.cpp
@@ -1,42 +1,13 @@
-//This is an example abot how to commit and push my code
-
-#include<iostream>
+#include <iostream>
+#include "BookCollection.h"
 
 using namesapce std;
 
 
-class BookCollection 
+int main()
 {
-   private:
-      int section[4];      
+   BookCollation SectionA(4,5,6,7);
+   BookCollation SectionB(2,3,5,7);
 
-
-   public:
-     BookCollection(int a, int b, int c, int d)
-     {
-        section[0] = a;
-        section[1] = b;
-        section[2] = c;
-        section[3] = d;
-    }
-
-    BookCollection operator+(const BookCollection& other) const {
-        return BookCollection(
-            section[0] + other.section[0],   
-            section[1] + other.section[1],   
-            section[2] + other.section[2],   
-            section[3] + other.section[3],
-        );
-    }
-
-};
-
-
-// sectionA = [4,5,6,7] 
-// sectionB = [1,2,3,4]
-
-
-// SectionC = sectionA + sectionB
-
-// SectionC = [5,7,9,11]
-
+   SectionC = SectionA + SectionB;
+}
